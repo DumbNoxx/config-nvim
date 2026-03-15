5a3cf5b0b9a5189d6e567e4b749344cbf34e5102
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2026-03-15 01:14:26 -0400

    chore

--- lua/sethy/plugins/pulse.lua
+++ lua/sethy/plugins/pulse.lua
@@ -1,9 +1,4 @@
 return {
 	"DumbNoxx/pulse.nvim",
-	opts = {
-		server_url = "beam-1.onrender.com:443",
-		idle_time = 30000,
-		validate = "1e93cba66f1e3b587057d6c1d9d1ae8049a2a677e9a9e298d75ec2ec1b4478d1",
-		isLocalhost = "false",
-	},
+	opts = {},
 }
